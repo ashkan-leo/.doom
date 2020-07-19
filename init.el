c8f687e6b28660b67443f8f64f81d429b80fc298
Ashkan Aleali <ashkan.aleali@gmail.com>
2020-07-19 16:54:43 -0500

    Add configs

---
+++ init.el
@@ -0,0 +1,149 @@
+;;; init.el -*- lexical-binding: t; -*-
+;; Copy me to ~/.doom.d/init.el or ~/.config/doom/init.el, then edit me!
+
+(doom! :completion
+
+       (company) ;; supports +childframe
+       (ivy
+        +prescient
+        +icons) ;; supports +childframe
+       ;; (helm)
+
+       :ui
+       workspaces
+       hydra
+       deft
+       doom
+       doom-dashboard
+       ophints
+       hl-todo
+       indent-guides
+       modeline
+       nav-flash
+       (treemacs)
+       (popup
+        +all
+        +defaults
+        +icons)
+       pretty-code
+       unicode
+       vc-gutter
+       vi-tilde-fringe
+       (window-select
+        +numbers)
+       zen
+
+       :editor
+       (evil +everywhere)
+       word-wrap
+       file-templates
+       snippets
+       fold
+       (format)
+       multiple-cursors
+       parinfer
+       rotate-text
+       lispy
+
+       :emacs
+       (dired
+        +icons)
+       electric
+       ibuffer
+       undo
+       vc
+
+       :term
+       eshell
+       vterm
+
+       :checkers
+       (syntax) ;; supports +childframe
+       spell
+
+       :tools
+       (lookup
+        +docsets
+        +dictionary)
+       (eval +overlay)
+       (debugger
+        +realgud)
+       direnv
+       docker
+       ;; ein
+       gist
+       (lsp +peek)
+       macos
+       (magit +forge)
+       make
+       pass
+       pdf
+       tmux
+       upload
+       biblio
+
+       :lang
+       yaml
+       json
+       data
+       emacs-lisp
+       (haskell +dante)
+       (java +meghanada)
+       javascript
+       julia
+       (latex
+        +latexmk
+        +cdlatex
+        +fold)
+       ledger
+       markdown
+       nix
+       (org
+        +present
+        +roam
+        +pomodoro
+        +pandoc
+        +noter
+        +jupyter
+        +dragndrop
+        +hugo
+        +brain
+        +gnuplot)
+       plantuml
+       purescript
+       (python
+        +lsp)
+       racket
+       rest
+       (scala +lsp)
+       (sh +fish)
+       web
+
+       :app
+       ;;(email +gmail)    ; emacs as an email client
+       ;;irc               ; how neckbeards socialize
+       (rss +org)        ; emacs as an RSS reader
+       ;;twitter           ; twitter client https://twitter.com/vnought
+
+       :collab
+       ;;floobits          ; peer programming for a price
+       ;;impatient-mode    ; show off code over HTTP
+
+       :config
+       literate
+
+       (default
+         +bindings
+         +smartparens))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((dante-methods stack)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ packages.el
@@ -0,0 +1,33 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+
+;; org extensions
+(package! org-drill)
+(package! org-super-agenda)
+(package! org-fancy-priorities)
+(package! org-ref)
+(package! org-noter-pdftools)
+(package! org-roam-bibtex :recipe (:host github :repo "org-roam/org-roam-bibtex"))
+
+;; org exporters
+(package! ox-moderncv :recipe (:host gitlab :repo "Titan-C/org-cv"))
+(package! ox-altacv   :recipe (:host gitlab :repo "Titan-C/org-cv"))
+
+;; ui
+(package! nyan-mode)
+
+;; dash at point
+(package! counsel-dash)
+(package! dash-at-point)
+(package! helm-dash)
+
+;; tools
+(package! real-auto-save)
+(package! sphinx-doc)
+
+(unpin! org-roam company-org-roam)
+(unpin! bibtex-completion helm-bibtex ivy-bibtex)
+(unpin! lsp-mode)
+
+;; (package! exec-path-from-shell)
